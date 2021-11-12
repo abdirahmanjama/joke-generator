9c10df78c50846f1bd59b9dce352ceca79307b12
abdi.j <abdi.j@gembaadvantage.com>
2021-11-12 15:13:04 +0000

    set reference to customHook in parent component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import useRandomJoke from "./useRandomJoke";
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [joke, setJoke] = useState("");
-  useEffect(() => {
-    const fetchJoke = async () =>
-      await fetch(`http://api.icndb.com/jokes/random`)
-        .then((res) => res.json())
-        .then((data) => {
-          setJoke(data.value.joke);
-        });
+  const joke = useRandomJoke();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-    fetchJoke();
-  }, []);
-
-  const handleClick = (e: React.MouseEventHandler<HTMLButtonElement>) => {
-    setJoke();
-  };
   return (
     <div className="App">
       <h1>The Joke Generator</h1>
-      <button onClick={handleClick}>Generate Joke</button>
+
+      <form>
+        <input
+          placeholder="first name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          placeholder="last name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </form>
       <h3>{joke}</h3>
     </div>
   );
